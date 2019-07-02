0cebb776ecd7cd8b2816d1766e8130cf6e75b92d
KalebWorkneh <kalebzenawi@gmail.com>
2019-07-02 16:42:47 -0500

    Decorator

---
+++ Lab6_Decorate My Text/js/decoratemytext.js
@@ -0,0 +1,32 @@
+var flag = 0;
+
+function decorator(){
+    let mytext = document.getElementById("inputText");
+    let currentSize = parseInt(window.getComputedStyle(mytext).getPropertyValue("font-size"));
+    mytext.style.fontSize = (currentSize + 2) + "pt";
+}
+
+function decorateWithBtn(){
+    if(!flag){
+        clearInterval(myInterval);
+    } else {
+        myInterval();       
+    }
+}
+
+function myInterval(){
+    decorator();
+    setInterval(decorator, 2000);
+}
+
+function decorateWithCB(){
+    let mytext = document.getElementById("inputText");
+    let myBox = document.getElementById("myCheckBox");
+    if (myBox.checked){
+        mytext.style.fontWeight = "bold";
+        mytext.style.color = "green";
+        mytext.style.textDecoration = "underline";
+    }else{
+        mytext.style.fontWeight = "normal";
+    }
+}
