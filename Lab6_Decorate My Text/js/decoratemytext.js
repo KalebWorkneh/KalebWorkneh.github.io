efce1b9aafba045a86fc2466ab73b8b1268abc1e
KalebWorkneh <kalebzenawi@gmail.com>
2019-07-02 21:53:43 -0500

    pig update

--- Lab6_Decorate My Text/js/decoratemytext.js
+++ Lab6_Decorate My Text/js/decoratemytext.js
@@ -22,13 +22,34 @@ function decorate(){
 function decorateWithCB(){
     let mytext = document.getElementById("inputText");
     let myBox = document.getElementById("myCheckBox");
+    let wholeBody = document.getElementById("body1");
     if (myBox.checked){
         mytext.style.fontWeight = "bold";
         mytext.style.color = "green";
         mytext.style.textDecoration = "underline";
+        wholeBody.style.backgroundImage = "url('Images/hundred-dollar-bill.jpg')";
     }else{
         mytext.style.fontWeight = "";
         mytext.style.color = "";
         mytext.style.textDecoration = "";
+        wholeBody.style.backgroundImage = "";
     }
+}
+
+function pigLatin(){
+    let myText = document.getElementById("inputText");
+    var mySmallText = myText.value.toLowerCase();
+    var vowels = "aeiou";
+    var myNew = "";
+    var addText = "ay";
+    if(vowels.indexOf(mySmallText.charAt(0)) >= 0) {
+        addText = "way";
+    }
+    for(var i = 0; i < mySmallText.length; i++){
+        if(vowels.indexOf(mySmallText.charAt(i)) >= 0){
+            myNew = myNew + mySmallText.substr(i, mySmallText.length) + mySmallText.substr(0, i) + addText;
+            break;
+        }
+    }
+    myText.value = myNew;
 }
