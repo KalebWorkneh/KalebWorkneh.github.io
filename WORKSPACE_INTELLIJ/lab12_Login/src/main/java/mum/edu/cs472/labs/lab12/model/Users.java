632a38dac38ad3b210c92063f23c488c9bfb99ad
KalebWorkneh <kalebzenawi@gmail.com>
2019-07-13 01:42:18 -0500

    Lab 12

---
+++ WORKSPACE_INTELLIJ/lab12_Login/src/main/java/mum/edu/cs472/labs/lab12/model/Users.java
@@ -0,0 +1,29 @@
+package mum.edu.cs472.labs.lab12.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Users {
+
+    private List<User> users = new ArrayList<>();
+    {
+        users.add(new User("john","1234","John K. Dany"));
+        users.add(new User("merry","4231","Merry S. Thompson"));
+        users.add(new User("anna","0123","Anna K. Portski"));
+    }
+
+    public Users() {
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(User u){
+        users.add(u);
+    }
+}
